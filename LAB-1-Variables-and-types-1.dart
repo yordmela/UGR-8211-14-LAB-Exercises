9562da3d93f4cf590fb302e1ef95a19a2a916646
Yordanos <yordanos.mela@gmail.com>
2024-03-08 16:06:30 +0300

    LAB-1-Variables-and-types-1.dart

---
+++ LAB-1-Variables-and-types-1.dart
@@ -0,0 +1,6 @@
+void main(){
+  var name= "Yordanos Melaku";
+  var age = 20;
+  var favoriteColor="Blue";
+  print("My name is ${name}, i am ${age} year old, my favorite color is ${favoriteColor}");
+}
