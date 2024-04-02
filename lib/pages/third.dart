038a378b70be6f5b5095f5f3dc1ec0c25c14acc0
yordmela <119792940+yordmela@users.noreply.github.com>
2024-04-02 16:29:34 +0300

    Update third.dart

--- lib/pages/third.dart
+++ lib/pages/third.dart
@@ -1,10 +1,73 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class Third extends StatelessWidget{
   const Third({super.key});
 
   @override
   Widget build(BuildContext context){
-    return Scaffold();
+    return Scaffold(
+       backgroundColor: Colors.blue,
+      appBar: AppBar(
+      backgroundColor: Colors.purple,
+      title: Text("BMI CALCULATOR",
+      
+      ),
+      centerTitle: true,
+      
+      ),
+     
+      body:Container(
+        child:Column(
+        children:[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+           
+           Container(
+                width: 150,
+                height: 150,
+                color: Colors.black,
+                child: Center(
+                  child: Icon(Icons.female_outlined, size: 100.0,color:Colors.white)
+                ),
+              ),
+
+            Container(
+                width: 150,
+                height: 150,
+                color: Colors.black,
+                child: Center(
+                  child: Icon(Icons.male_outlined, size: 100.0,color:Colors.white)
+                ),
+              ),
+          ],),
+
+          SizedBox(height: 100.0,),
+           Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+           
+           Container(
+                width: 300,
+                height: 150,
+                color: Colors.black,
+                child: Center(
+                  child: Icon(Icons.female_outlined, size: 100.0,color:Colors.white)
+                ),
+              ),
+
+            
+          ],),
+           Text("a")
+        ])
+      ),
+
+      bottomNavigationBar: BottomAppBar(
+        
+        child:ElevatedButton(onPressed: () {
+  
+        }, child:Text("CALCULATE"),)),
+    );
   }
 }
